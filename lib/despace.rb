990ce1eb1652e8432b382105407668158fbd9592
Robin Simth <robinsmith@venus.local>
2014-07-22 21:48:03 -0700

    Auto-update

--- lib/despace.rb
+++ lib/despace.rb
@@ -10,7 +10,7 @@ require 'pp'
 APPNAME = "lastf"
 
 options = OpenStruct.new
-options[:pattern] = ENV['HOSTNAME']
+options[:pattern] = "*"
 
 opt_parser = OptionParser.new do |opts|
   opts.banner = "Usage: #{APPNAME} [options]"
@@ -33,9 +33,7 @@ opt_parser.parse(ARGV)
 awk_part = "awk '{ s = \"\"; for (i = 9; i <= NF; i++) s = s $i \" \" ; print s }'"
 
 # NEED TO CHANGE THIS TO A FIND COMMAND - LEARN HOW TO USE FIND!
-command = "ls *\" \"* | tail +1 | grep #{options.pattern}"
-
-files = `#{command}`.to_s.split("\n")
+files = `ls #{options.pattern}`.to_s.split("\n")
 
 files.each do |filename|
   if /\ /.match(filename) and !File.directory?(filename)
