6f1d1111ef7246ee88868dcc5b6fed6bbc59eb49
robinrob <robin@rsmith.io>
2015-10-17 14:22:59 +0100

    Quick-save.

---
+++ lib/template.rb
@@ -0,0 +1,32 @@
+#!/usr/bin/env ruby
+
+# Template for Ruby command-line tool
+
+require 'optparse'
+require 'optparse/time'
+require 'ostruct'
+require 'pp'
+require 'colorize'
+
+APPNAME = "lastf"
+
+options = OpenStruct.new
+options[:pattern] = "*"
+
+opt_parser = OptionParser.new do |opts|
+  opts.banner = "Usage: #{APPNAME} [options]"
+
+  opts.separator "Specific options:"
+
+  opts.on("-p", "--pattern [PATTERN]",
+          "Pattern to de-space.") do |pattern|
+    options.pattern = pattern
+  end
+
+  opts.on("-h", "--help", "Show this message") do
+    puts opts
+    exit
+  end
+end
+
+opt_parser.parse(ARGV)
